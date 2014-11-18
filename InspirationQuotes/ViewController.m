334447483d1994fd14399d8b9f2ffd2e660c3a1f
TheTaier <oneander@gmail.com>
2014-11-18 14:37:41 +0200

    Initial Commit

---
+++ InspirationQuotes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InspirationQuotes
+//
+//  Created by Deniss Kaibagarovs on 11/18/14.
+//  Copyright (c) 2014 Deniss Kaibagarovs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
